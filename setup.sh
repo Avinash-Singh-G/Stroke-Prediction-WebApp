8ffff5d25528b156073f9207999610a77f269ba0
Avinash-Singh-G <74002584+Avinash-Singh-G@users.noreply.github.com>
2021-04-09 00:22:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
-# shellcheck disable=SC2028
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" >~/.streamlit/credentials.toml
-# shellcheck disable=SC2028
+
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-" >~/.streamlit/config.toml
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
